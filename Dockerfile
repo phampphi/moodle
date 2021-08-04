da4b501d0762874b65594827107210591bd86121
Phi Pham <pham.pphi@gmail.com>
2021-08-05 08:15:34 +1000

    Update deployment

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM erseco/alpine-php7-webserver
 LABEL maintainer "Phi Pham <pham.pphi@gmail.com>"
-ENV MOODLE_VERSION=3.10.4
 
 COPY --chown=nobody rootfs /
 
@@ -10,7 +9,7 @@ RUN apk update && \
     chown nobody:nobody /usr/sbin/crond && chmod +x /etc/service/cron/run && \
     setcap cap_setgid=ep /usr/sbin/crond && \
 	# Install 'php-redis'
-	apk --no-cache add php7-pecl-redis && \
+	#apk --no-cache add php7-pecl-redis && \
 	chmod +x /var/www/html/admin/cli/*
 
 USER nobody
--- rootfs/docker-entrypoint-init.d/01-setup.sh
+++ rootfs/docker-entrypoint-init.d/01-setup.sh
@@ -18,7 +18,7 @@ if [ ! -f /var/www/html/config.php ]; then
     echo "Generating config.php file..."
     ENV_VAR='var' php -d max_input_vars=1000 /var/www/html/admin/cli/install.php \
         --lang=${MOODLE_LANGUAGE:-en} \
-        --wwwroot=${SITE_URL:-http://localhost:8080} \
+        --wwwroot=${SITE_URL:-http://127.0.0.1:8080} \
         --dataroot=${DATAROOT:-/var/www/moodledata/} \
         --dbtype=${DB_TYPE:-mysqli} \
         --dbhost=$DB_HOST \
@@ -36,8 +36,23 @@ if [ ! -f /var/www/html/config.php ]; then
         --agree-license \
         --skip-database
 
+    sed -i '/require_once/i $CFG->localcachedir="/var/www/html/localcache";' /var/www/html/config.php
+
     if [ "$SSLPROXY" = 'true' ]; then
         sed -i '/require_once/i $CFG->sslproxy=true;' /var/www/html/config.php
+    fi
+
+    if [[ -n "${MOODLE_REDIS_HOST}" ]]; then
+        sed -i '/require_once/i $CFG->session_handler_class = "\core\session\redis"' /var/www/html/config.php
+        sed -i '/require_once/i $CFG->session_redis_host = "${MOODLE_REDIS_HOST}"' /var/www/html/config.php
+        sed -i '/require_once/i $CFG->session_redis_port = 6379' /var/www/html/config.php
+        sed -i '/require_once/i $CFG->session_redis_database = 0' /var/www/html/config.php
+        sed -i '/require_once/i $CFG->session_redis_auth = "${MOODLE_REDIS_PASSWORD}"' /var/www/html/config.php
+        sed -i '/require_once/i $CFG->session_redis_prefix = "${MOODLE_REDIS_PREFIX:-}"' /var/www/html/config.php
+        sed -i '/require_once/i $CFG->session_redis_acquire_lock_timeout = 120' /var/www/html/config.php
+        sed -i '/require_once/i $CFG->session_redis_acquire_lock_retry = 100' /var/www/html/config.php
+        sed -i '/require_once/i $CFG->session_redis_lock_expire = 7200' /var/www/html/config.php
+        sed -i '/require_once/i $CFG->session_redis_serializer_use_igbinary = false' /var/www/html/config.php
     fi
 fi
 
@@ -54,4 +69,14 @@ if php -d max_input_vars=1000 /var/www/html/admin/cli/isinstalled.php ; then
         --agree-license
 else
     echo "Database installed."
+fi
+
+if [[ -n "${DISABLED_PLUGINS}" ]]; then
+    echo "Disabling plugins: ${DISABLED_PLUGINS}"
+    php /var/www/html/admin/cli/uninstall_plugins.php  --plugins=${DISABLED_PLUGINS} --run
+fi
+
+if [[ -n "${OLD_SITE_URL}" && "${OLD_SITE_URL}" != "${SITE_URL}" ]]; then
+    echo "Change SiteURL: ${OLD_SITE_URL} --> ${SITE_URL}"
+    php /var/www/html/admin/tool/replace/cli/replace.php --search=${OLD_SITE_URL} --replace=${SITE_URL} --non-interactive
 fi
